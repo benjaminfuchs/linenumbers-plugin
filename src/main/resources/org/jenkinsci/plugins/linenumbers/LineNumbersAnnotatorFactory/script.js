a45a00dc3c0e68981b1ca13555ef038fac5be08e
Benjamin Fuchs <email@benjaminfuchs.de>
2017-08-06 14:53:53 +0200

    Merging all snapshot changes

--- src/main/resources/org/jenkinsci/plugins/linenumbers/LineNumbersAnnotatorFactory/script.js
+++ src/main/resources/org/jenkinsci/plugins/linenumbers/LineNumbersAnnotatorFactory/script.js
@@ -2,12 +2,6 @@
 (function () {
     "use strict";
 
-    Behaviour.specify('div.fold', 'fold.toggle', 10, function (element) {
-        element.onclick = function () {
-            element.toggleClassName('open');
-        };
-    });
-
     function scrollToElement(element) {
         window.scrollTo(0, element.offsetTop - 30);
     }
